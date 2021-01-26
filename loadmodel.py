4464291fe0909eb5af93fec84093903f51f2f902
sairaj18 <37768344+sairaj18@users.noreply.github.com>
2021-01-26 10:09:39 +0530

    Update loadmodel.py
    made compatable with tensorflow version 2.0

--- loadmodel.py
+++ loadmodel.py
@@ -2,17 +2,26 @@ import os
 import sys
 import numpy as np
 from keras.models import load_model
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import pickle
 from keras.preprocessing.sequence import pad_sequences
+tf.disable_eager_execution()
+from tensorflow.python.keras.backend import set_session
+
+sess = tf.Session()
 graph = tf.get_default_graph()
 
 def loading_model():
-    model=load_model('sentiment.h5')
-    token=pickle.load(open('token.pickle','rb'))
-    print('model loaded')
-    return model,token
+	global sess
+	set_session(sess)
+	model=load_model('sentiment.h5')
+	token=pickle.load(open('token.pickle','rb'))
+	print('model loaded')
+	return model,token
 def predcit(model,token,text):
+	global sess
+	global graph
+	set_session(sess)
 	hh = token.texts_to_sequences([text])
 	hh = pad_sequences(hh, maxlen=29)
 	hh=np.array(hh[0]).reshape(1,29)
